a157df0aa2a4abbe159949c58b49f0466c74ebfa
afroCoderHanane <gbadhanane@gmail.com>
2022-04-23 14:42:33 -0700

    this is a login page

--- app/src/main/java/com/example/sampleapp/MainActivity.java
+++ app/src/main/java/com/example/sampleapp/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.sampleapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction(View view) {
+        EditText usernameLogin = findViewById(R.id.usernameLogin);
+        EditText passwordLogin = findViewById(R.id.passwordLogin);
+        Log.i("Info", usernameLogin.getText().toString());
+        Log.i("Info", passwordLogin.getText().toString());
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
