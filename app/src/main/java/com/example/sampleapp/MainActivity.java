ec642be0c3d2ad4522e297ebf027c1af3a393612
afroCoderHanane <gbadhanane@gmail.com>
2022-04-23 15:37:28 -0700

    added logo

--- app/src/main/java/com/example/sampleapp/MainActivity.java
+++ app/src/main/java/com/example/sampleapp/MainActivity.java
@@ -6,14 +6,17 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    public void clickFunction(View view) {
+    public void login(View view) {
         EditText usernameLogin = findViewById(R.id.usernameLogin);
         EditText passwordLogin = findViewById(R.id.passwordLogin);
         Log.i("Username", usernameLogin.getText().toString());
         Log.i("Password", passwordLogin.getText().toString());
+        Toast.makeText(this, "Button pressed " + usernameLogin.getText().toString(), Toast.LENGTH_LONG).show();
+
     }
 
     @Override
